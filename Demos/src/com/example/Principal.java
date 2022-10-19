e5642a4ca7b5499c814860a4f0c2a03f289cd99c
Javier <jmartin@grupoloyal.com>
2022-10-19 19:41:11 +0200

    Variables

--- Demos/src/com/example/Principal.java
+++ Demos/src/com/example/Principal.java
@@ -10,11 +10,43 @@ public class Principal {
 
 	/**
 	 * Método principal
+	 * 
 	 * @param args Argumentos de la linea de comandos
 	 */
 	public static void main(String[] args) {
 		Principal app = new Principal();
-		System.out.println(app.suma(2, 2));
+		// System.out.println(app.suma(2, 2));
+		app.variables();
+	}
+
+	public void variables() {
+		int i = 0, j, k = 0;
+		final int id;
+		String s = "\t\tHola mundo\n" + "que tal";
+		s = """
+				Hola mundo
+				que tal
+				""";
+		s.length();
+		id = i * k;
+		j = 1;
+		k = i + j;
+		var x = 0b1001_1100_0011;
+		System.out.println(x);
+		x = 4;
+		i += x;
+		long l;
+		l = i;
+		i = (int) l;
+		i = 0;
+		Object o = new Principal();
+		o = "cadena";
+		if (o instanceof Principal) {
+			((Principal) o).suma(k, id);
+		}
+		i = Integer.parseInt("44");
+		s = Integer.toString(i);
+
 	}
 
 	/**
